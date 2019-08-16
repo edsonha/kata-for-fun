3d710f8c515fcc2d5b918b60d442fd9270ac038b
edsonha <edsonha@gmail.com>
2019-08-16 09:35:38 +0800

    Finish fixed length queue data structure

--- dataStructure/fixedLengthQueue/fixedLengthQueue.js
+++ dataStructure/fixedLengthQueue/fixedLengthQueue.js
@@ -1,11 +1,34 @@
 module.exports = class FixedLengthQueue {
-  constructor(length) {}
+  constructor(length) {
+    this.fixedQueue = new Array(length);
+    this.enqueueSlot = 0;
+    this.dequeueSlot = 0;
+    this.length = length;
+  }
 
-  getSize() {}
+  getSize() {
+    return this.length;
+  }
 
-  enqueue(item) {}
+  enqueue(item) {
+    if (this.enqueueSlot > this.length - 1) {
+      throw new Error();
+    } else {
+      this.fixedQueue[this.enqueueSlot] = item;
+      this.enqueueSlot++;
+      return this.fixedQueue;
+    }
+  }
 
-  dequeue() {}
+  dequeue() {
+    if (this.dequeueSlot < this.enqueueSlot) {
+      const dequeueItem = this.fixedQueue[this.dequeueSlot];
+      this.dequeueSlot++;
+      return dequeueItem;
+    }
+  }
 
-  peek() {}
+  peek() {
+    return this.fixedQueue[this.dequeueSlot];
+  }
 };
--- dataStructure/fixedLengthQueue/fixedLengthQueue.test.js
+++ dataStructure/fixedLengthQueue/fixedLengthQueue.test.js
@@ -7,75 +7,87 @@ describe("FixedLengthQueue", () => {
       expect(fixedQueue.getSize()).toBe(3);
     });
   });
-  // it('should create an array of length passed in', () => {
-  //   const fixedQueue = new FixedLengthQueue(3);
-  //   expect(fixedQueue.getSize()).toEqual(3);
-  // });
 
-  // it('can enqueue and dequeue', () => {
-  //   const fixedQueue = new FixedLengthQueue(3);
-  //   fixedQueue.enqueue('apple');
-  //   expect(fixedQueue.dequeue()).toEqual('apple');
-  // });
+  describe("enqueue", () => {
+    it("should add item into the fixedQueue", () => {
+      const fixedQueue = new FixedLengthQueue(3);
+      expect(fixedQueue.enqueue("A")).toEqual(["A", undefined, undefined]);
+    });
 
-  // it('can maintain the order', () => {
-  //   const fixedQueue = new FixedLengthQueue(3);
-  //   fixedQueue.enqueue('apple');
-  //   fixedQueue.enqueue('banana');
-  //   fixedQueue.enqueue('citrus');
-  //   expect(fixedQueue.dequeue()).toEqual('apple');
-  //   expect(fixedQueue.dequeue()).toEqual('banana');
-  //   expect(fixedQueue.dequeue()).toEqual('citrus');
-  //   expect(fixedQueue.dequeue()).toEqual(undefined);
-  // });
+    it("should add item into the fixedQueue in order", () => {
+      const fixedQueue = new FixedLengthQueue(3);
+      expect(fixedQueue.enqueue("A")).toEqual(["A", undefined, undefined]);
+      expect(fixedQueue.enqueue("B")).toEqual(["A", "B", undefined]);
+      expect(fixedQueue.enqueue("C")).toEqual(["A", "B", "C"]);
+    });
 
-  // it('cannot insert more than the max length', () => {
-  //   const fixedQueue = new FixedLengthQueue(2);
-  //   fixedQueue.enqueue('apple');
-  //   fixedQueue.enqueue('banana');
-  //   expect(() => fixedQueue.enqueue('citrus')).toThrow(Error);
-  // });
+    it("should throw an error when adding an item while fixedQueue is full", () => {
+      const fixedQueue = new FixedLengthQueue(2);
+      fixedQueue.enqueue("A");
+      fixedQueue.enqueue("B");
+      expect(() => fixedQueue.enqueue("C")).toThrow(Error);
+    });
+  });
 
-  // it('cannot insert more than the max length even after dequeue', () => {
-  //   const fixedQueue = new FixedLengthQueue(2);
-  //   fixedQueue.enqueue('apple');
-  //   fixedQueue.enqueue('banana');
-  //   fixedQueue.dequeue();
-  //   expect(() => fixedQueue.enqueue('citrus')).toThrow(Error);
-  // });
+  describe("dequeue", () => {
+    it("should be able to enqueue and dequeue", () => {
+      const fixedQueue = new FixedLengthQueue(2);
+      fixedQueue.enqueue("A");
+      expect(fixedQueue.dequeue()).toBe("A");
+    });
 
-  // it('can peek', () => {
-  //   const fixedQueue = new FixedLengthQueue(2);
-  //   expect(fixedQueue.peek()).toEqual(undefined);
-  //   fixedQueue.enqueue('apple');
-  //   fixedQueue.enqueue('banana');
-  //   expect(fixedQueue.peek()).toEqual('apple');
-  //   expect(fixedQueue.dequeue()).toEqual('apple');
-  //   expect(fixedQueue.peek()).toEqual('banana');
-  // });
+    it("should be able to enqueue and dequeue in order", () => {
+      const fixedQueue = new FixedLengthQueue(2);
+      fixedQueue.enqueue("A");
+      fixedQueue.enqueue("B");
+      expect(fixedQueue.dequeue()).toBe("A");
+      expect(fixedQueue.dequeue()).toBe("B");
+      expect(fixedQueue.dequeue()).toBe(undefined);
+    });
 
-  // it('can peek correctly after dequeue', () => {
-  //   const fixedQueue = new FixedLengthQueue(3);
+    it("should not be able to insert item more than the max length even after dequeue", () => {
+      const fixedQueue = new FixedLengthQueue(2);
+      fixedQueue.enqueue("A");
+      fixedQueue.enqueue("B");
+      fixedQueue.dequeue();
+      expect(() => fixedQueue.enqueue("C")).toThrow(Error);
+    });
+  });
 
-  //   expect(fixedQueue.dequeue()).toEqual(undefined);
-  //   expect(fixedQueue.peek()).toEqual(undefined);
+  describe("peek", () => {
+    it("should be able to peek at the first real item in the queue", () => {
+      const fixedQueue = new FixedLengthQueue(2);
+      expect(fixedQueue.peek()).toBe(undefined);
+      fixedQueue.enqueue("A");
+      fixedQueue.enqueue("B");
+      expect(fixedQueue.peek()).toEqual("A");
+      expect(fixedQueue.dequeue()).toEqual("A");
+      expect(fixedQueue.peek()).toEqual("B");
+    });
 
-  //   fixedQueue.enqueue('apple');
-  //   expect(fixedQueue.peek()).toEqual('apple');
-  //   expect(fixedQueue.dequeue()).toEqual('apple');
-  //   expect(fixedQueue.peek()).toEqual(undefined);
-  //   expect(fixedQueue.dequeue()).toEqual(undefined);
+    it("can peek correctly after dequeue", () => {
+      const fixedQueue = new FixedLengthQueue(3);
 
-  //   fixedQueue.enqueue('banana');
-  //   expect(fixedQueue.peek()).toEqual('banana');
-  //   expect(fixedQueue.dequeue()).toEqual('banana');
-  //   expect(fixedQueue.peek()).toEqual(undefined);
-  //   expect(fixedQueue.dequeue()).toEqual(undefined);
+      expect(fixedQueue.dequeue()).toEqual(undefined);
+      expect(fixedQueue.peek()).toEqual(undefined);
 
-  //   fixedQueue.enqueue('citrus');
-  //   expect(fixedQueue.peek()).toEqual('citrus');
-  //   expect(fixedQueue.dequeue()).toEqual('citrus');
-  //   expect(fixedQueue.peek()).toEqual(undefined);
-  //   expect(fixedQueue.dequeue()).toEqual(undefined);
-  // });
+      fixedQueue.enqueue("apple");
+      expect(fixedQueue.peek()).toEqual("apple");
+      expect(fixedQueue.dequeue()).toEqual("apple");
+      expect(fixedQueue.peek()).toEqual(undefined);
+      expect(fixedQueue.dequeue()).toEqual(undefined);
+
+      fixedQueue.enqueue("banana");
+      expect(fixedQueue.peek()).toEqual("banana");
+      expect(fixedQueue.dequeue()).toEqual("banana");
+      expect(fixedQueue.peek()).toEqual(undefined);
+      expect(fixedQueue.dequeue()).toEqual(undefined);
+
+      fixedQueue.enqueue("citrus");
+      expect(fixedQueue.peek()).toEqual("citrus");
+      expect(fixedQueue.dequeue()).toEqual("citrus");
+      expect(fixedQueue.peek()).toEqual(undefined);
+      expect(fixedQueue.dequeue()).toEqual(undefined);
+    });
+  });
 });
