ead3cc03fcc108c3cc6992e981ce2751647b8483
edsonha <edsonha@gmail.com>
2020-02-19 15:20:11 +0800

    Refactoring: create multiple loops, one for each concern

--- kata/videostore-refactoring-without-OOP/rentalCalculator.js
+++ kata/videostore-refactoring-without-OOP/rentalCalculator.js
@@ -34,13 +34,13 @@ function statement(customer, movies) {
   let result = `Rental Record for ${customer.name}\n`;
   for (let rental of customer.rentals) {
     const rentalBill = calculateBillForRental(movies, rental);
-
+    result += `\t${getMovie(movies, rental).title}\t${rentalBill}\n`;
+    totalBill += rentalBill;
+  }
+  for (let rental of customer.rentals) {
     frequentRenterPoints++;
     if (getMovie(movies, rental).code === "new" && rental.days > 2)
       frequentRenterPoints++;
-
-    result += `\t${getMovie(movies, rental).title}\t${rentalBill}\n`;
-    totalBill += rentalBill;
   }
 
   result += `Amount owed is ${totalBill}\n`;
