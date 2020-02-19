57d19a220ad21f5e43176ccf8cf24e5ab0c1fdcf
edsonha <edsonha@gmail.com>
2020-02-19 17:08:18 +0800

    Add domain model classes: Customer, Movies, Rental

---
+++ kata/videostore-refactoring-with-OOP/Customer.js
@@ -0,0 +1,12 @@
+class Customer {
+  constructor(name) {
+    this.name = name;
+    this.frequentRenterPoints = 0;
+  }
+
+  incrementRentalPoints() {
+    this.frequentRenterPoints++;
+  }
+}
+
+module.exports = Customer;
---
+++ kata/videostore-refactoring-with-OOP/Rental.js
@@ -0,0 +1,8 @@
+class Rental {
+  constructor(movies, days) {
+    this.movies = movies;
+    this.days = days;
+  }
+}
+
+module.exports = Rental;
