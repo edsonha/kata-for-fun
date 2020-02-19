c9284e967b79b5d74ab2c2b24ab4afe76b13e626
edsonha <edsonha@gmail.com>
2020-02-19 14:55:19 +0800

    Extract method: separate bill calculation and the print result method

--- kata/videostore-refactoring-without-OOP/rentalCalculator.js
+++ kata/videostore-refactoring-without-OOP/rentalCalculator.js
@@ -2,41 +2,45 @@ function getMovie(movies, rental) {
   return movies[rental.movieID];
 }
 
+function calculateBillForRental(movies, rental) {
+  let rentalBill = 0;
+  if (getMovie(movies, rental) === undefined) {
+    throw new Error(`Invalid movieID`);
+  }
+
+  switch (getMovie(movies, rental).code) {
+    case "regular":
+      rentalBill = 2;
+      if (rental.days > 2) {
+        rentalBill += (rental.days - 2) * 1.5;
+      }
+      break;
+    case "new":
+      rentalBill = rental.days * 3;
+      break;
+    case "childrens":
+      rentalBill = 1.5;
+      if (rental.days > 3) {
+        rentalBill += (rental.days - 3) * 1.5;
+      }
+      break;
+  }
+  return rentalBill;
+}
+
 function statement(customer, movies) {
   let totalAmount = 0;
   let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
   for (let rental of customer.rentals) {
-    let thisAmount = 0;
-
-    if (getMovie(movies, rental) === undefined) {
-      throw new Error(`Invalid movieID`);
-    }
-
-    switch (getMovie(movies, rental).code) {
-      case "regular":
-        thisAmount = 2;
-        if (rental.days > 2) {
-          thisAmount += (rental.days - 2) * 1.5;
-        }
-        break;
-      case "new":
-        thisAmount = rental.days * 3;
-        break;
-      case "childrens":
-        thisAmount = 1.5;
-        if (rental.days > 3) {
-          thisAmount += (rental.days - 3) * 1.5;
-        }
-        break;
-    }
+    let rentalBill = calculateBillForRental(movies, rental);
 
     frequentRenterPoints++;
     if (getMovie(movies, rental).code === "new" && rental.days > 2)
       frequentRenterPoints++;
 
-    result += `\t${getMovie(movies, rental).title}\t${thisAmount}\n`;
-    totalAmount += thisAmount;
+    result += `\t${getMovie(movies, rental).title}\t${rentalBill}\n`;
+    totalAmount += rentalBill;
   }
 
   result += `Amount owed is ${totalAmount}\n`;
