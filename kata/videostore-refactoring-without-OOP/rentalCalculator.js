60508ccbac78a14b28aa45d05ad623246eb021ed
edsonha <edsonha@gmail.com>
2020-02-19 14:48:16 +0800

    Refactoring: replace temp with query(to prepare for extract method)

--- kata/videostore-refactoring-without-OOP/rentalCalculator.js
+++ kata/videostore-refactoring-without-OOP/rentalCalculator.js
@@ -1,43 +1,44 @@
+function getMovie(movies, rental) {
+  return movies[rental.movieID];
+}
+
 function statement(customer, movies) {
   let totalAmount = 0;
   let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
-  for (let r of customer.rentals) {
-    let movie = movies[r.movieID];
+  for (let rental of customer.rentals) {
     let thisAmount = 0;
 
-    if (movie === undefined) {
+    if (getMovie(movies, rental) === undefined) {
       throw new Error(`Invalid movieID`);
     }
-    // determine amount for each movie
-    switch (movie.code) {
+
+    switch (getMovie(movies, rental).code) {
       case "regular":
         thisAmount = 2;
-        if (r.days > 2) {
-          thisAmount += (r.days - 2) * 1.5;
+        if (rental.days > 2) {
+          thisAmount += (rental.days - 2) * 1.5;
         }
         break;
       case "new":
-        thisAmount = r.days * 3;
+        thisAmount = rental.days * 3;
         break;
       case "childrens":
         thisAmount = 1.5;
-        if (r.days > 3) {
-          thisAmount += (r.days - 3) * 1.5;
+        if (rental.days > 3) {
+          thisAmount += (rental.days - 3) * 1.5;
         }
         break;
     }
 
-    //add frequent renter points
     frequentRenterPoints++;
-    // add bonus for a two day new release rental
-    if (movie.code === "new" && r.days > 2) frequentRenterPoints++;
+    if (getMovie(movies, rental).code === "new" && rental.days > 2)
+      frequentRenterPoints++;
 
-    //print figures for this rental
-    result += `\t${movie.title}\t${thisAmount}\n`;
+    result += `\t${getMovie(movies, rental).title}\t${thisAmount}\n`;
     totalAmount += thisAmount;
   }
-  // add footer lines
+
   result += `Amount owed is ${totalAmount}\n`;
   result += `You earned ${frequentRenterPoints} frequent renter points\n`;
 
